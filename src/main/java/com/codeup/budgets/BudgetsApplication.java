4446f8c43e2afb57cfd9df9130943d17d867c2d0
KennethMcInturf <kennethcmcinturf@gmail.com>
2019-01-14 07:25:12 -0600

    first commit

---
+++ src/main/java/com/codeup/budgets/BudgetsApplication.java
@@ -0,0 +1,14 @@
+package com.codeup.budgets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetsApplication.class, args);
+    }
+
+}
+
